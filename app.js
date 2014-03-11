d6ff45e0d1bfcdc11f8a8b3ed3e1b3e8d0e90e0c
Alexander Ose <alexose@Alexanders-MacBook-Pro.local>
2014-03-11 13:11:25 -0400

    fix server, add API scaffold

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var http = require("http")
   , url = require("url")
+  , fs = require("fs")
+  , SubAPI = require("./lib/api")
   , cache = require("memory-store");
 
 var port = process.argv && process.argv.length > 2 ? process.argv[2] : 3000;
 
 http.createServer(function(request, response) {
 
-  var queries = querystring.parse(request.url.split('?')[1]);
+  var arr = request.url.split('?');
+
+  if (arr[1]){
+    var queries = querystring.parse(arr[1]);
+
+    new SubAPI(queries, finish);
+  } else {
+    explain();
+  }
 
   function error(string){
-    respond(response, string, "text", 500);
+    respond(string, "text", 500);
   }
 
   function finish(results){
@@ -22,32 +32,35 @@ http.createServer(function(request, response) {
       string = queries.callback + '(' + string + ')';
     }
 
-    respond(response, string, type);
+    respond(string, type);
   }
+
+  function explain(){
+
+    // Load HTML template
+    try{
+      fs.readFile('./README.md', 'utf8', function(err, md){
+        respond(md);
+      });
+    } catch(e){
+      respond(e.toString(), 500);
+    }
+  }
+
+  function respond(string, type, code){
+
+    type = type || "text/plain";
+    code = code || 200;
+
+    response.writeHead(code, {
+      "Content-Type": type
+    });
+    response.write(string);
+    response.end();
+  }
+
 }).listen(port, function(){
   console.log('Server running on port ' + port);
 });
 
-function explain(request, response){
 
-  // Load HTML template
-  try{
-    fs.readFile('README.md', 'utf8', function(err, html){
-      respond(response, html);
-    });
-  } catch(e){
-    respond(response, 'Could not find Sieve library.  Did you run npm install?');
-  }
-}
-
-function respond(response, string, type, code){
-
-  type = type || "text/html";
-  code = code || 200;
-
-  response.writeHead(code, {
-    "Content-Type": type
-  });
-  response.write(string);
-  response.end();
-}
